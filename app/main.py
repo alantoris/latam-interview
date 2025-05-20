d1e211c809e3226562e1cbd60d3db818fedb516c
alantoris <alan.toris@gmail.com>
2025-05-20 18:26:07 -0300

    Do not import routes temporarily

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from app.routers import user_router
+#from app.routers import user_router
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-app.include_router(user_router)
+#app.include_router(user_router)
