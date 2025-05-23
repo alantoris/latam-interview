8307725385ce2c001cb48bfb027707f79a9291b5
alantoris <alan.toris@gmail.com>
2025-05-22 21:02:25 -0300

    Change DATABASE URL

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,4 @@ COPY . .
 # Expose the expected port
 EXPOSE 8080
 
-COPY start.sh /start.sh
-RUN chmod +x /start.sh
-
-# Usamos el script como comando de arranque
-CMD ["/start.sh"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8080"]
--- app/db/database.py
+++ app/db/database.py
@@ -9,8 +9,12 @@ DB_USER = os.getenv("POSTGRES_USER")
 DB_PASS = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("POSTGRES_HOST")
 DB_NAME = os.getenv("POSTGRES_DB")
+ENV = os.getenv("ENV", "DEV")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+if ENV == "PROD":
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@/{DB_NAME}?host={DB_HOST}"
+else:
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
